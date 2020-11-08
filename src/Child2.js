bdeb142f2c9481ab565b9ae450d80825263048f5
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-09 04:14:02 +0500

    updated Child2.js
    imported Child2.js in Parent.js .
    In Child2.js imported Reducer and passed its value to h3 and added a button which increments state value by 1

--- src/Child2.js
+++ src/Child2.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import CounterReducer from './CounterReducer'
 
 const Child2 = () =>{
+
+    let [state, dispatch] = useReducer(CounterReducer, 10);
+
     return(
         <div>
-            
+            <h1>value of Reducer is : {state} </h1>
+            <button onClick={()=> dispatch('INCREMENT') } >Increment Reducer</button>
         </div>
     );
 }
