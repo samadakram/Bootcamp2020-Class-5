eb3e9453029f4fe336d9173544503682b521ce4a
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-09 02:52:49 +0500

    updated App.js
    removed counterContext.Provider from App.js passed value directly from CounterContext to Child.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Parent from './Parent.js';
 import './App.css';
-import counterContext from './CounterContext.js';
 
 function App() {
   return (
-      <counterContext.Provider value={10} >
       <div>
         <Parent />
       </div>
-    </counterContext.Provider>
   );
 }
 
