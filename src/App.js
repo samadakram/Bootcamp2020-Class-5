ce9bf1cc8a7980ffbe01caac6a28daecca11a27a
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-09 02:58:29 +0500

    Update App.js
    again imported counterContext and Wraped all components between tag of counterContext.Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Parent from './Parent.js';
+import counterContext from './CounterContext';
 import './App.css';
 
 function App() {
   return (
+    <counterContext.Provider value= {25} >
       <div>
         <Parent />
       </div>
+    </counterContext.Provider>
   );
 }
 
