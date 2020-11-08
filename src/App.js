7569b75b7fc87d4659187b6d34409d673bc025cf
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-08 21:12:37 +0500

    added new components
    Added Parent.js and Child.js component and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+import Parent from './Parent.js';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Parent />
     </div>
   );
 }
---
+++ src/Parent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Child from './Child.js';
+
+function Parent(){
+    return(
+        <div>
+            <Child />
+        </div>
+    );
+}
+
+export default Parent;
